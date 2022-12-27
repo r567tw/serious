cc558e0452e2a58c12b5bf4a7fefe3fd9e56a0c1
r567tw <r567tw@gmail.com>
2022-12-27 10:38:29 +0800

    Column adjusted

--- app/Filament/Resources/CourseResource/Widgets/CourseOverview.php
+++ app/Filament/Resources/CourseResource/Widgets/CourseOverview.php
@@ -7,10 +7,22 @@ use Filament\Widgets\StatsOverviewWidget\Card;
 
 class CourseOverview extends BaseWidget
 {
+    protected int | string | array $columnSpan = [
+        'md' => 3,
+        'xl' => 3,
+    ];
+
+    protected function getColumns(): int
+    {
+        return 3;
+    }
+
     protected function getCards(): array
     {
         return [
             Card::make('Totoal Course', \App\Models\Course::count()),
+            Card::make('Totoal Voted', \App\Models\Vote::count()),
+            Card::make('Totoal User', \App\Models\User::count()),
         ];
     }
 }
